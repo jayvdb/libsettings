3f03ba0252c4afb1136c4be86521678c123a7769
John Vandenberg <jayvdb@gmail.com>
2021-06-14 17:45:09 +0800

    shell: cmd

--- rust/build.rs
+++ rust/build.rs
@@ -29,6 +29,7 @@ fn main() {
     println!("cargo:rustc-link-lib=static=swiftnav");
     println!("cargo:rustc-link-lib=static=rustbindsettings");
 
+    // ldflags https://github.com/alexcrichton/cmake-rs/issues/55
     println!("cargo:rustc-link-search=native=c:/Miniconda/envs/test/Library/lib");
     println!("cargo:rustc-link-lib=static=pthreads_static");
 
