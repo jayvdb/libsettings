8ae73606b0d1984dac67740f4b4813486835f949
John Vandenberg <jayvdb@gmail.com>
2021-08-10 17:26:24 +0800

    locks.c: Reformat

--- rust/libsettings_locks.c
+++ rust/libsettings_locks.c
@@ -14,35 +14,36 @@
 
 #ifdef _MSC_VER
 
-#define MS_PER_SEC      1000ULL     // MS = milliseconds
-#define US_PER_MS       1000ULL     // US = microseconds
-#define HNS_PER_US      10ULL       // HNS = hundred-nanoseconds (e.g., 1 hns = 100 ns)
-#define NS_PER_US       1000ULL
+#define MS_PER_SEC 1000ULL  // MS = milliseconds
+#define US_PER_MS 1000ULL   // US = microseconds
+#define HNS_PER_US 10ULL    // HNS = hundred-nanoseconds (e.g., 1 hns = 100 ns)
+#define NS_PER_US 1000ULL
 
-#define HNS_PER_SEC     (MS_PER_SEC * US_PER_MS * HNS_PER_US)
-#define NS_PER_HNS      (100ULL)    // NS = nanoseconds
-#define NS_PER_SEC      (MS_PER_SEC * US_PER_MS * NS_PER_US)
+#define HNS_PER_SEC (MS_PER_SEC * US_PER_MS * HNS_PER_US)
+#define NS_PER_HNS (100ULL)  // NS = nanoseconds
+#define NS_PER_SEC (MS_PER_SEC * US_PER_MS * NS_PER_US)
 
 #include <windows.h>
 
-int clock_gettime_realtime(struct timespec *tv)
-{
-    FILETIME ft;
-    ULARGE_INTEGER hnsTime;
+int clock_gettime_realtime(struct timespec *tv) {
+  FILETIME ft;
+  ULARGE_INTEGER hnsTime;
 
-    GetSystemTimeAsFileTime(&ft);
+  GetSystemTimeAsFileTime(&ft);
 
-    hnsTime.LowPart = ft.dwLowDateTime;
-    hnsTime.HighPart = ft.dwHighDateTime;
+  hnsTime.LowPart = ft.dwLowDateTime;
+  hnsTime.HighPart = ft.dwHighDateTime;
 
-    // To get POSIX Epoch as baseline, subtract the number of hns intervals from Jan 1, 1601 to Jan 1, 1970.
-    hnsTime.QuadPart -= (11644473600ULL * HNS_PER_SEC);
+  // To get POSIX Epoch as baseline, subtract the number of hns intervals from
+  // Jan 1, 1601 to Jan 1, 1970.
+  hnsTime.QuadPart -= (11644473600ULL * HNS_PER_SEC);
 
-    // modulus by hns intervals per second first, then convert to ns, as not to lose resolution
-    tv->tv_nsec = (long) ((hnsTime.QuadPart % HNS_PER_SEC) * NS_PER_HNS);
-    tv->tv_sec = (long) (hnsTime.QuadPart / HNS_PER_SEC);
+  // modulus by hns intervals per second first, then convert to ns, as not to
+  // lose resolution
+  tv->tv_nsec = (long)((hnsTime.QuadPart % HNS_PER_SEC) * NS_PER_HNS);
+  tv->tv_sec = (long)(hnsTime.QuadPart / HNS_PER_SEC);
 
-    return 0;
+  return 0;
 }
 #endif
 
